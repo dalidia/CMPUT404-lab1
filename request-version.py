4adb29babfc162e2a01566fb1bfd5052b980d77a
Lidia Ataupillco <ataupill@ualberta.ca>
2022-01-10 20:07:18 -0500

    finished lab 1

--- request-version.py
+++ request-version.py
@@ -1,4 +1,7 @@
 import requests
 
 print("Version of requests library", requests.__version__)
+
 google_page = requests.get("http://google.com/")
+py_script = requests.get("https://raw.githubusercontent.com/dalidia/CMPUT404-lab1/main/request-version.py")
+print(py_script.text)
